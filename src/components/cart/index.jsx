eb9c780eeeda1691ebb88b8e57dae5a1ad8c96b4
Luiz Calaça <luizcalaca@gmail.com>
2022-12-22 10:58:29 -0300

    feat: calculating total price

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,11 +1,13 @@
 import * as Styles from "./styles";
 import { useSelector } from "react-redux";
 import CartItem from '../cart-item/index'
+import {selectProductsTotalPrice} from '../../redux/cart/cart.selectors'
 
 const Cart = ({ isVisible, setIsVisible }) => {
   const handleEscapeAreaClick = () => setIsVisible(false);
   const {products} = useSelector((rootReducer)=>rootReducer.cartReducer);
-  
+  const productsTotalPrice = useSelector(selectProductsTotalPrice)
+
   return (
     <Styles.CartContainer isVisible={isVisible}>
       <Styles.CartEscapeArea onClick={handleEscapeAreaClick} />
@@ -13,6 +15,8 @@ const Cart = ({ isVisible, setIsVisible }) => {
         <Styles.CartTitle>Seu Carrinho</Styles.CartTitle>
 
         {products.map(product => <CartItem product={product}/> )}
+
+        <Styles.CartTotal>R${productsTotalPrice}</Styles.CartTotal>
       </Styles.CartContent>
     </Styles.CartContainer>
   );
