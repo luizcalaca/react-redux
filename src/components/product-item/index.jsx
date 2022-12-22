a00e5fd343a324d6abfaa4d654edd07f7f69ba8e
Luiz Calaça <luizcalaca@gmail.com>
2022-12-22 09:22:35 -0300

    feat: adding a handling click for add product

--- src/components/product-item/index.jsx
+++ src/components/product-item/index.jsx
@@ -1,18 +1,20 @@
 import { BsCartPlus } from "react-icons/bs";
-
-// Components
 import CustomButton from "../custom-button/index";
-
-// Styles
 import * as Styles from "./styles";
-
-// Utilities
+import { useDispatch } from "react-redux";
+import { addProductToCart } from "../../redux/cart/actions";
 
 const ProductItem = ({ product }) => {
+  const dispatch = useDispatch()
+
+  const handleProductClick = () => {
+    dispatch(addProductToCart(product))
+  }
+
   return (
     <Styles.ProductContainer>
       <Styles.ProductImage imageUrl={product.imageUrl}>
-        <CustomButton startIcon={<BsCartPlus />}>
+        <CustomButton startIcon={<BsCartPlus />} onClick={handleProductClick}>
           Adicionar ao carrinho
         </CustomButton>
       </Styles.ProductImage>
